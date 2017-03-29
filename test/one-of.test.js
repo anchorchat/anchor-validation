ff43cfb5733f36c1a207496547034ea647931c82
Lars Tadema <lars.tadema@anchor.chat>
2017-03-29 14:45:01 +0200

    added oneOf validation and default error = true if no error message is specified

---
+++ test/one-of.test.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import { oneOf, isEqual, isAlphanumeric, isEmail } from '../src';
+
+describe('oneOf', () => {
+  it('should return undefined if both validations pass', () => {
+    const isEqualValidation = isEqual('test', 'test');
+    const isAlphanumericValidation = isAlphanumeric('test', 'test');
+    expect(oneOf(isEqualValidation, isAlphanumericValidation, 'error_one_of')).to.equal(undefined);
+  });
+
+  it('should return true if validation fails and no error message is specified', () => {
+    const isEqualValidation = isEqual('test', 'test');
+    const isAlphanumericValidation = isAlphanumeric('test@!', 'test');
+    expect(oneOf(isEqualValidation, isAlphanumericValidation)).to.equal(true);
+  });
+
+  it('should return error message if validation fails', () => {
+    const isEqualValidation = isEqual('test', 'test');
+    const isAlphanumericValidation = isAlphanumeric('test@!', 'test');
+    expect(oneOf(isEqualValidation, isAlphanumericValidation, 'error_one_of')).to.equal('error_one_of');
+  });
+});
