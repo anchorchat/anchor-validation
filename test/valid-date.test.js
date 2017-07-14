ec53a4f3d51db8ce16c7933946b18e1113af2ca0
Lars Tadema <lars.tadema@anchor.chat>
2017-07-14 15:40:02 +0200

    added valiDate validation test

---
+++ test/valid-date.test.js
@@ -0,0 +1,21 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import { validDate } from '../src';
+
+describe('validDate', () => {
+  it('should return validDate function if only a date of birth is provided', () => {
+    expect(validDate('1991-05-12')).to.be.a('function');
+  });
+
+  it('should return error message if validation fails', () => {
+    expect(validDate('YYYY-MM-DD')('05-12-1991', 'error_date')).to.equal('error_date');
+  });
+
+  it('should return true if validation fails and no error message is specified', () => {
+    expect(validDate('YYYY-MM-DD')('05-12-1991')).to.equal(true);
+  });
+
+  it('should return undefined if value passes check', () => {
+    expect(validDate('YYYY-MM-DD')('1990-10-10', 'error_date')).to.equal(undefined);
+  });
+});
